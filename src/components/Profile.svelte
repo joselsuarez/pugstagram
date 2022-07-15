82ce77d6083270db8045054947c14cfd853f79f1
Jose Suarez <jsuarez@hangarcentral.com>
2022-07-15 17:13:14 -0400

    netlify

---
+++ src/components/Profile.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let nickname;
+  export let name;  
+</script>
+
+<style>
+    .Profile-content {
+      display: flex;
+      align-items: center;
+    }
+    .Profile-avatar img {
+      width: 42px;
+      height: 42px;
+      border-radius: 50%;
+    }
+    .Profile-info {
+      margin: 0 0 0 0.5em;
+    }
+    .Profile-info h2 {
+      font-size: 14px;
+      color: black;
+      margin: 0;
+      padding: 0;
+    }
+    .Profile-info span {
+      font-size: 12px;
+      font-weight: normal;
+    }
+</style>
+
+<div class="Profile">
+    <div class="Profile-content">
+        <div class="Profile-avatar">
+            <img src="https://titles.trackercdn.com/valorant-api/agents/7f94d92c-4234-0a36-9646-3a87eb8b5c89/displayicon.png" alt="" />
+        </div>
+        <div class="Profile-info">
+            <h2>{nickname}</h2>
+            <span>{name}</span>
+        </div>
+    </div>
+</div>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Profile from './Profile.svelte';
+    import Stories from './Stories';
+    import Footer from './Footer';
+
+    export let nickname;
+    export let name;
+
+</script>
+
+<style>
+    .Sidebar {
+      position: relative;
+      padding: 4.5em 0 0 0;
+    }
+    .Sidebar-container {
+      position: fixed;
+    }
+</style>
+
+<div class="Sidebar">
+    <div class="Sidebar-container">
+        <Profile {nickname} {name} />
+        <Stories />
+        <Footer />
+    </div>
+</div>
